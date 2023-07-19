9651baada9231dbf445be4b88f9f3d7b76e48b13
Luis Miranda <131805357+feltfelipe@users.noreply.github.com>
2023-07-19 00:09:18 -0300

    chuteumnumero.py

--- chute_numero.py
+++ chute_numero.py
@@ -1,28 +1,3 @@
-'''2. CHUTE O NÚMERO
-Objetivo: Criar um script que gerá um valor aleatoriamente, guardar este valor, 
-e perguntar repetidamente para o usuário chutar o valor gerado até que ele acerte.
-
-Habilidades praticas a aplicar:
-
--Random
--Comparadores matemáticos
--Controle de Fluxo
--Entrada de dados
--Saida de dados
-
-Detalhes e boas Práticas: Você deve criar um projeto para rodar na linha de comando que ao iniciar,
-irá gerar, armazenar, porem não exibir um valor aleatório entre um valor mínimo e máximo que será 
-definido por você ( 10-100, por exemplo). Com esse valor gerado e armazenado de alguma forma que 
-você (o criador ou jogador do script) não possa ver, faça uma pergunta do tipo: “Chute um número” 
-para quem estiver rodando o script e com isso o programa deve gravar a resposta que foi passada.
-
-Depois disso você terá 3 caminhos possíveis: 1. Avisar que a pessoa chutou baixo, 2 dizer que chutou
-alto ou parabenizar dizer que acertou! Considerando os três possíveis caminhos que podem ser seguidos,
-você deve cuidar para que em todo o momento a entrada de dados seja tratado para exceções e que caso 
-o usuário digite algo inesperado, que ele receba uma mensagem amigável o informando das possíveis opções 
-que seu programa oferece. Isso deve continuar acontecendo indefinitivamente até que a pessoa acerte o 
-número ou desista por não conseguir acertar hahaha(acontece).'''
-
 import random
 from time import sleep
 
